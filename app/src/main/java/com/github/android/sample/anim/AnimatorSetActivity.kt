0c4793bfde95aca9afcd467eb2fcc8c555a509d5
zhaoyu57 <zhaoyu1@jd.com>
2018-03-22 13:29:16 +0800

    AnimatorSet

--- app/src/main/java/com/github/android/sample/anim/AnimatorSetActivity.kt
+++ app/src/main/java/com/github/android/sample/anim/AnimatorSetActivity.kt
@@ -3,17 +3,24 @@ package com.github.android.sample.anim
 import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.better.base.ToolbarActivity
+import com.better.base.model.SampleItem
 import com.github.android.sample.R
 import kotlinx.android.synthetic.main.activity_animator_set.*
 
-class AnimatorSetActivity : AppCompatActivity() {
+class AnimatorSetActivity : ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animator_set)
-
+        // 设置 toolbar
+        intent.getParcelableExtra<SampleItem<Activity>>("item")?.let {
+            supportActionBar?.title = it.title
+            supportActionBar?.subtitle = it.desc
+        }
 
         val animator1 = ObjectAnimator.ofFloat(text1, "translationY", 0f, 300f, 0f).setDuration(1000)
         val animator2 = ObjectAnimator.ofFloat(text2, "rotation", 0f, -180f, +180f, 0f).setDuration(1000)
@@ -51,6 +58,24 @@ class AnimatorSetActivity : AppCompatActivity() {
                     override fun onAnimationStart(animation: Animator?) {
                     }
                 })
+            }.start()
+        }
+
+        // AnimatorSet.Builder会覆盖具体的ObjectAnimator设置
+        btn_builder_target.setOnClickListener {
+            AnimatorSet().apply {
+                duration = 5000     // 覆蓋
+                playTogether(animator1,animator2,animator3)
+                setTarget(text1)    // 這個setTarget必須放在這裡，放在playTogether之前無效
+            }.start()
+        }
+
+        // delay
+        btn_delay.setOnClickListener {
+            animator1.startDelay = 2000
+            AnimatorSet().apply {
+                startDelay = 2000
+                play(animator2).with(animator1)
             }.start()
         }
     }
