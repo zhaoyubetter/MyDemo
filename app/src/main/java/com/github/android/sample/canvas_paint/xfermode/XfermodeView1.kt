cba357377fa29b04c9cef0f38b0f8cbaa0ec2824
zhaoyubetter@126.com <zhaoyubetter@126.com>
2018-04-12 08:40:33 +0800

    xfermode

--- app/src/main/java/com/github/android/sample/canvas_paint/xfermode/XfermodeView1.kt
+++ app/src/main/java/com/github/android/sample/canvas_paint/xfermode/XfermodeView1.kt
@@ -4,10 +4,6 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import android.graphics.Bitmap
-import android.graphics.RectF
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
 
 
 /**
@@ -16,8 +12,8 @@ import android.graphics.PorterDuffXfermode
 
 class XfermodeView1(ctx: Context, attrs: AttributeSet? = null) : View(ctx, attrs) {
 
-    private val wid = 150
-    private val hei = 150
+    private val wid = 100
+    private val hei = 100
     private lateinit var dstBmp: Bitmap
     private lateinit var srcBmp: Bitmap
     private var paint: Paint
@@ -36,45 +32,76 @@ class XfermodeView1(ctx: Context, attrs: AttributeSet? = null) : View(ctx, attrs
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SRC))
 
         canvas.save()
-        canvas.translate(260f, 0f)
+        canvas.translate(180f, 0f)
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP))
 
-        canvas.translate(260f, 0f)
+        canvas.translate(180f, 0f)
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
 
-        canvas.translate(260f, 0f)
+        canvas.translate(180f, 0f)
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SRC_OUT))
+
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SRC_OVER))
 
         canvas.restore()
         // ==== 1 end
 
         canvas.save()
-        canvas.translate(0f, 260f)
-        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SRC_OVER))
-
-        canvas.translate(260f, 0f)
+        canvas.translate(0f, 180f)
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST))
 
-        canvas.translate(260f, 0f)
+        canvas.translate(180f, 0f)
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST_ATOP))
 
-        canvas.translate(260f, 0f)
+        canvas.translate(180f, 0f)
         mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST_IN))
+
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST_OUT))
+
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST_OVER))
         canvas.restore()
         ///////=== 2 end
 
         canvas.save()
 
-        canvas.translate(0f, 260 * 2f)
-        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST_OUT))
+        canvas.translate(0f, 180f*2)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.ADD))
 
-        canvas.translate(260f, 260 * 2f)
-        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DST_OVER))
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.MULTIPLY))
 
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.LIGHTEN))
+
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.OVERLAY))
+
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.SCREEN))
+
+        canvas.restore()
+        //////3===end
+
+        canvas.save()
+        canvas.translate(0f, 180f*3)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.DARKEN))
+        canvas.restore()
+
+
+        canvas.save()
+        canvas.translate(0f, 180f * 4f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.CLEAR))
+        canvas.translate(180f, 0f)
+        mydraw(canvas, PorterDuffXfermode(PorterDuff.Mode.XOR))
+
+        canvas.restore()
     }
 
     private fun mydraw(canvas: Canvas, mode: PorterDuffXfermode) {
-        canvas.drawRect(10f, 10f, 255f, 255f, paint)  // 150 + 75 = 235
+        canvas.drawRect(10f, 10f, 180f, 180f, paint)  // 150 + 30 (10+10+10) = 180
         canvas.save()
         canvas.translate(10f, 10f)
         canvas.save()
