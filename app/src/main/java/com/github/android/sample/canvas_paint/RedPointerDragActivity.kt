d91826bbb3f366795ff9a3b89f98f0a145c2faea
zhaoyubetter <zhaoyubetter@126.com>
2018-04-18 21:44:01 +0800

    小红点效果

--- app/src/main/java/com/github/android/sample/canvas_paint/RedPointerDragActivity.kt
+++ app/src/main/java/com/github/android/sample/canvas_paint/RedPointerDragActivity.kt
@@ -6,7 +6,11 @@ import android.widget.Toolbar
 import com.better.base.ToolbarActivity
 import com.github.android.sample.R
 import com.github.android.sample.canvas_paint.redPointer.RedPointerView1
+import com.github.android.sample.canvas_paint.redPointer.RedPointerView2
+import com.github.android.sample.canvas_paint.redPointer.RedPointerView3
+import com.github.android.sample.canvas_paint.redPointer.RedPointerView4
 import kotlinx.android.synthetic.main.activity_red_pointer_drag.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class RedPointerDragActivity : ToolbarActivity() {
 
@@ -14,7 +18,30 @@ class RedPointerDragActivity : ToolbarActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_red_pointer_drag)
 
+
         root_container.removeAllViews()
-        root_container.addView(RedPointerView1(this))
+        root_container.addView(RedPointerView1(this@RedPointerDragActivity))
+
+        btn_base.onClick {
+            root_container.removeAllViews()
+            root_container.addView(RedPointerView1(this@RedPointerDragActivity))
+        }
+
+        btn_base_2.onClick {
+            root_container.removeAllViews()
+            root_container.addView(RedPointerView2(this@RedPointerDragActivity))
+        }
+
+        btn_base_3.onClick {
+            root_container.removeAllViews()
+            root_container.addView(RedPointerView3(this@RedPointerDragActivity))
+        }
+
+        btn_base_4.onClick {
+            root_container.removeAllViews()
+            root_container.addView(RedPointerView4(this@RedPointerDragActivity))
+        }
     }
+
+
 }
