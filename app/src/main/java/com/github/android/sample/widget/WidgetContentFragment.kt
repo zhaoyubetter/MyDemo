4b107560e9a06b00b0a3161c9122b31d0fc71c08
zhaoyubetter <zhaoyubetter@126.com>
2018-04-25 13:34:38 +0800

    目录调整

---
+++ app/src/main/java/com/github/android/sample/widget/WidgetContentFragment.kt
@@ -0,0 +1,71 @@
+package com.github.android.sample.widget
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.better.base.holder.Type1Holder
+import com.better.base.model.SampleItem
+import com.github.android.sample.BaseFunItemTemplate
+import com.github.android.sample.R
+import com.github.better.recycler.*
+import org.jetbrains.anko.find
+
+/**
+ * Created by zhaoyu on 2018/3/9.
+ */
+class WidgetContentFragment : Fragment() {
+    private lateinit var recycler: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_basic_content, container, false)
+        recycler = view.find(R.id.recycler)
+        return view
+    }
+
+    private fun getData(pid: Int?, datas: MutableList<ExpandNode<SampleItem<Activity>>>, level: Int = 0):
+            MutableList<ExpandNode<SampleItem<Activity>>>? {
+        if (pid == null) {
+            return null
+        }
+        var level = level
+        // 模板数据
+        val curItems = WidgetFunItemTemplate.getInstance(activity)[pid]
+        // ExpandNode
+        val curExpandNodes = mutableListOf<ExpandNode<SampleItem<Activity>>>()
+        curItems?.forEach { it ->
+            val node = ExpandNode(it, 0, level == 1)
+            curExpandNodes.add(node)                // 父节点
+            // 孩子节点
+            getData(it.id, mutableListOf(), level++)?.let {
+                node.addChildren(it)
+            }
+        }
+        datas.addAll(curExpandNodes)
+        return datas
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val datas = mutableListOf<ExpandNode<SampleItem<Activity>>>()
+        getData(0, datas)
+
+        recycler.layoutManager = LinearLayoutManager(activity)
+        recycler.adapter = ExpandAdapter(recycler, datas, object : ExpandHolderFactory<SampleItem<Activity>> {
+            override fun getHolder(helper: ExpandRecyclerViewHelper<SampleItem<Activity>>,
+                                   parent: ViewGroup, viewType: Int): ExpandViewHolder<SampleItem<Activity>> {
+                return when (viewType) {
+                    0 -> Type1Holder(helper, LayoutInflater.from(context).inflate(R.layout.type_item_1, parent, false))
+                    1 -> Type1Holder(helper, LayoutInflater.from(context).inflate(R.layout.type_item_2, parent, false))
+                    2 -> Type1Holder(helper, LayoutInflater.from(context).inflate(R.layout.type_item_3, parent, false))
+                    else -> Type1Holder(helper, LayoutInflater.from(context).inflate(R.layout.type_item_1, parent, false))
+                }
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/github/android/sample/widget/WidgetFunItemTemplate.kt
@@ -0,0 +1,62 @@
+package com.github.android.sample.widget
+
+import android.app.Activity
+import android.content.Context
+import com.better.base.model.SampleItem
+import com.github.android.sample.BaseFunItemTemplate
+import com.github.android.sample.anim.*
+import com.github.android.sample.canvas_paint.*
+import com.github.android.sample.widget.viewgroup.FlowLayoutActivity
+
+/**
+ * Created by zhaoyu on 2018/3/11.
+ */
+class WidgetFunItemTemplate private constructor(ctx: Context) {
+
+    // 伴生对象
+    companion object {
+        private var instance: WidgetFunItemTemplate? = null
+        private val items = mutableListOf<SampleItem<Activity>>()
+        private val groupsItems = mutableMapOf<Int, List<SampleItem<Activity>>>()
+
+        fun getInstance(context: Context): WidgetFunItemTemplate {
+            if (instance == null)
+                instance = WidgetFunItemTemplate(context)
+            return instance!!
+        }
+
+        private fun item(closure: SampleItem<Activity>.() -> Unit) {
+            items.add(SampleItem<Activity>().apply(closure))
+        }
+
+        // 分组模板
+        private fun group(closure: () -> Unit) {
+            closure.invoke()
+            groupsItems += items.groupBy { it.pid }     // 根据pid进行分组
+        }
+    }
+
+    operator fun get(id: Int?) = groupsItems[id]
+    operator fun contains(id: Int?) = groupsItems.any { it.key == id }
+
+    init {
+        group {
+            // 第一部分 动画
+            item {
+                pid = 0
+                id = 1
+                title = "自定义控件-ViewGroup"
+                desc = "ViewGroup自定义控件"
+
+                item {
+                    pid = 1
+                    id = 11
+                    title = "FlowLayout - 流布局"
+                    desc = "FlowLayout"
+                    clazz = FlowLayoutActivity::class.java
+
+                } // end 13
+            }
+        }
+    }
+}
