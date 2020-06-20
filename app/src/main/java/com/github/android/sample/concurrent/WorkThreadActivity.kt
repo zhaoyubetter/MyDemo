ce7d1020945833861b9b8403e57f80c82878bec6
zhaoyubetter <zhaoyubetter@126.com>
2020-06-20 16:56:09 +0800

    workThread demo

--- app/src/main/java/com/github/android/sample/concurrent/WorkThreadActivity.kt
+++ app/src/main/java/com/github/android/sample/concurrent/WorkThreadActivity.kt
@@ -1,5 +1,6 @@
 package com.github.android.sample.concurrent
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import com.better.base.ToolbarActivity
@@ -15,6 +16,8 @@ class WorkThreadActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_thread)
+
+        AsyncTask
 
         workThread.startService()
 
